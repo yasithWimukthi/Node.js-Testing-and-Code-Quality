8256e335eaa9b1aef60a10a1bd24162c13ae9ef4
yasith <it19966922@my.sliit.lk>
2022-12-11 12:04:43 +0530

    add eslint

---
+++ public/.eslintrc.js
@@ -0,0 +1,14 @@
+({
+  "parserOptions": {
+    "sourceType": "script"
+  },
+  "env": {
+    "browser": true,
+    "jquery": true,
+    "node": false
+  },
+  "rules": {
+    "no-var": "off",
+    "prefer-const": "off"
+  }
+});
